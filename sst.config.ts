413797d3b223b30adf3fd55a23a82063491755ad
Abraham Kinney <mfp4074@gmail.com>
2023-10-25 16:37:33 -0700

    Setting up our React app

--- sst.config.ts
+++ sst.config.ts
@@ -2,6 +2,7 @@ import { ApiStack } from "./stacks/ApiStack";
 import { SSTConfig } from "sst";
 import { StorageStack } from "./stacks/StorageStack";
 import { AuthStack } from "./stacks/AuthStack";
+import { FrontendStack } from "./stacks/FrontendStack";
 
 export default {
   config(_input) {
@@ -11,6 +12,10 @@ export default {
     };
   },
   stacks(app) {
-    app.stack(StorageStack).stack(ApiStack).stack(AuthStack);
+    app
+      .stack(StorageStack)
+      .stack(ApiStack)
+      .stack(AuthStack)
+      .stack(FrontendStack);
   },
 } satisfies SSTConfig;
